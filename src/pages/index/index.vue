10598185cb480814fc89ecf27b890a8d2f1f5bae
chushuang <1214106564@qq.com>
2023-08-22 23:03:57 +0800

    ADD:分类和热门推荐

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,15 +2,21 @@
 import { ref } from 'vue'
 import { onLoad } from '@dcloudio/uni-app'
 import CustomNavbar from './components/CustomNavbar.vue'
-import { getHomeBannerAPI } from '@/services/home'
-import type { BannerItem } from '@/types/home'
+import CategoryPanel from './components/CategoryPanel.vue'
+import HotPanel from './components/HotPanel.vue'
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 
 // 变量
 const bannerList = ref<BannerItem[]>([])
+const categoryList = ref<CategoryItem[]>([])
+const hotList = ref<HotItem[]>([])
 
 // 生命周期
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategoryData()
+  getHomeHotData()
 })
 
 // 轮播图
@@ -18,14 +24,30 @@ const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   bannerList.value = res.result
 }
+
+// 分类
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
+
+// 热门推荐
+const getHomeHotData = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
 </script>
 
 <template>
   <CustomNavbar />
   <XtxSwiper :list="bannerList" />
+  <CategoryPanel :list="categoryList" />
+  <HotPanel :list="hotList" />
   <view class="index"> index </view>
 </template>
 
-<style lang="scss" scoped>
-//
+<style lang="scss">
+page {
+  background-color: #f7f7f7;
+}
 </style>
